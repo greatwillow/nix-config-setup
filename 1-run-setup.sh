d298f3461e2752ca1306a1bceef9854454c95703
Greg Denys <gregory.g.denys@gmail.com>
2022-10-29 14:50:47 -0400

    add debug statements

--- 1-run-setup.sh
+++ 1-run-setup.sh
@@ -34,13 +34,15 @@ create_new_user() {
 	print_line "Please enter a new user name:"
 	read user_name
 
-	if [ $is_linux_os == "true "]; then 
+	if [ $is_linux_os == "true " ]; then 
 		sudo useradd -m $user_name							# Add New User
 		sudo usermod -a -G sudo $user_name					# Add New User to sudoers group
 		sudo passwd $user_name								# Prompt for password for new user
 	fi
 
-	if [ $is_mac_os == "true "]; then 
+	print_line "before creating mac user ------------ "
+	if [ $is_mac_os == "true " ]; then 
+		print_line "creating mac user ------------ "
 		dscl . -create /Users/$user_name					# Add New User
 		print_line "Please enter a new user password:"		# Prompt for password for new user
 		read user_password
